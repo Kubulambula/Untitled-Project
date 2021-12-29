6a2a02fe5601d98e82c3d3f31580136f2f62e247
Kubulambula <jansta.ja@gmail.com>
2021-12-29 19:32:33 +0100

    fix various minor stuff

--- Scripts/UI/init.gd
+++ Scripts/UI/init.gd
@@ -7,13 +7,16 @@ func _ready():
 	ResourceQueue.register_callback(next_scene_path, "is_ready", funcref(self, "_next_scene_ready"))
 	ResourceQueue.queue_resource(next_scene_path)
 	if OS.is_debug_build():
-		$AnimationPlayer.play("intro_middle")
+		$AnimationPlayer.play("intro_fast")
 	else:
-		$AnimationPlayer.play("intro_slow")
+		$AnimationPlayer.play("intro_middle")
 
 
 func _next_scene_ready(resource):
 	if $AnimationPlayer.playback_active:
 		yield($AnimationPlayer, "animation_finished")
+	WindowOverlay.dim(true)
+	yield(get_tree().create_timer(.1), "timeout")
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene_to(ResourceQueue.get_resource(resource))
+	WindowOverlay.undim()
