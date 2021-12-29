45b04bec1c23ffe6bc24aded739392d435091a87
Skulaurun Mrusal <Skulaurun@seznam.cz>
2021-12-29 19:46:50 +0100

    Add player spawn

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -5,4 +5,9 @@ func _ready():
 		LevelManager.read_level_data("level1")
 	)
 	LevelManager.build_tilemaps(self, level_data["map"])
+	
+	var player = load("res://Scenes/Entities/Player.tscn").instance()
+	player.set_global_position(LevelManager.get_world_position(level_data["player"]["position"]))
+	add_child(player)
+	
 	print(level_data)
