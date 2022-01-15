ba28d623789107c1546219e69165639bf32bdefe
Kubulambula <jansta.ja@gmail.com>
2022-01-15 21:18:18 +0100

    window_overlay rework

--- Scripts/Singletons/window_overlay.gd
+++ Scripts/Singletons/window_overlay.gd
@@ -1,23 +1,43 @@
 extends Node
 
-var overlay_preload = preload("res://Scenes/Singleton_scenes/window_overlay.tscn")
-var overlay
-var _player: AnimationPlayer
+signal complete
+
+const _overlay_preload = preload("res://Scenes/Singleton_scenes/window_overlay.tscn")
+
+var default_duration: float = 0.5
+var dim_color: Color = Color("#242424")
+var undim_color: Color = Color("#00000000")
+
+var overlay: CanvasLayer
+var _tween: Tween
+var _color_rect: ColorRect
+
 
 
 func _ready():
-	overlay = overlay_preload.instance()
-	_player = overlay.get_node("AnimationPlayer")
+	overlay = _overlay_preload.instance()
+	_tween = overlay.get_node("Tween")
+# warning-ignore:return_value_discarded
+	_tween.connect("tween_all_completed", self, "_tween_completed")
+	_color_rect = overlay.get_node("ColorRect")
+	_color_rect.color = undim_color
 	add_child(overlay)
 
 
-func dim(instantly:bool = false):
-	_player.play("dim")
-	if instantly:
-		_player.seek(_player.current_animation_length)
+func dim(duration:float=0):
+	# warning-ignore:return_value_discarded
+	_tween.interpolate_property(_color_rect, "color", undim_color, dim_color, duration)
+	# warning-ignore:return_value_discarded
+	_tween.start()
 
 
-func undim(instantly:bool = false):
-	_player.play_backwards("dim")
-	if instantly:
-		_player.seek(0)
+func undim(duration:float=0):
+	# warning-ignore:return_value_discarded
+	_tween.interpolate_property(_color_rect, "color", dim_color, undim_color, duration)
+	# warning-ignore:return_value_discarded
+	_tween.start()
+
+
+func _tween_completed():
+	print("completed")
+	emit_signal("complete")
--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -5,17 +5,22 @@ func _ready():
 		LevelManager.read_level_data("level1")
 	)
 
-	
+
 	LevelManager.build_tilemaps(self, level_data["map"])
 	LevelManager.spawn_entities(self, level_data["entities"])
-	
+
 	print(level_data)
 	
 	WebAPI.login("hulan", "valecek123", funcref(self, "on_login"))
-	WebAPI.get_user_info(funcref(self, "on_get_user_info"))
+#	WebAPI.get_user_info(funcref(self, "on_get_user_info"))
+
 
 func on_login(code, response):
 	print("GET(" + str(code) + "): " + str(response))
+	if code == 200:
+		WebAPI.get_user_info(funcref(self, "on_get_user_info"))
+	else:
+		print("Non 200 code: ", code)
 
 # warning-ignore:unused_argument
 func on_get_user_info(code, response):
