3e61689d5c91d570b6169de2ddd1b41d1c7e5603
Kubulambula <jansta.ja@gmail.com>
2022-01-24 15:48:21 +0100

    Fix "not rendering" issue
    In reality I'm just an idiot and the issue was that the animation was too quick for the coroutine

--- Scripts/UI/init.gd
+++ Scripts/UI/init.gd
@@ -2,21 +2,39 @@ extends Node2D
 
 const next_scene_path = "res://Scenes/UI/main_menu.tscn"
 
+var intro = "intro_slow"
+
+var can_load_next_scene
+var next_scene = null
+
 
 func _ready():
 	ResourceQueue.register_callback(next_scene_path, "is_ready", funcref(self, "_next_scene_ready"))
 	ResourceQueue.queue_resource(next_scene_path)
 	if OS.is_debug_build():
-		$AnimationPlayer.play("intro_fast")
-	else:
-		$AnimationPlayer.play("intro_slow")
+		intro = "intro_fast"
+	$AnimationPlayer.play(intro)
 
 
 func _next_scene_ready(resource):
-	if $AnimationPlayer.playback_active:
-		yield($AnimationPlayer, "animation_finished")
-	WindowOverlay.dim(0)
+	if can_load_next_scene:
+		_switch_to_next_scene(ResourceQueue.get_resource(resource))
+	else:
+		next_scene = ResourceQueue.get_resource(resource)
+
+
+func animation_call():
+	if next_scene:
+		_switch_to_next_scene(next_scene)
+	can_load_next_scene = true
+
+
+func _switch_to_next_scene(scene):
+	if intro == "intro_fast":
+		WindowOverlay.dim(.1)
+	else:
+		WindowOverlay.dim(0)
 	yield(get_tree().create_timer(.1), "timeout")
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene_to(ResourceQueue.get_resource(resource))
+	get_tree().change_scene_to(scene)
 	WindowOverlay.undim(.3)
