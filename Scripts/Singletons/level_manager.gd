ff94b0a1bc09a23d5ca0bcb78be071cccf57fe5b
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-01-28 19:42:56 +0100

    Bug fixes in Level Manager

--- Scripts/Singletons/level_manager.gd
+++ Scripts/Singletons/level_manager.gd
@@ -119,24 +119,42 @@ func find_tile(map, tile):
 				return Vector2(x, y)
 	return Vector2(-1, -1)
 
+func count_tiles(map):
+	var counts = {}
+	for y in len(map):
+		for x in len(map[y]):
+			var character = map[y][x]
+			if not character in counts:
+				counts[character] = 0
+			counts[character] += 1
+	return counts
+
 # Možná jestli se bude s TileMapou hýbat, tak to bude dělat bordel, ale to se asi nebude (pro jistotu sem píšu ať vím kde hledat)
 func get_world_position(map_position):
 	return _default_tilemap.map_to_world(map_position)
 
-func apply_immovable_mask(level_data, mask, replace_with=" "):
+func apply_immovable_mask(level_data, mask):
+	# For simplicity's sake
 	var internal_level_data = _load_internal_level_data(level_data["level"])
 	var internal_map = internal_level_data["map"]
 	var internal_entities = internal_level_data["entities"]
 	var map = level_data["map"]
 	var entities = level_data["entities"]
+	# Correct tiles
 	for y in len(map):
 		for x in len(map[y]):
 			if map[y][x] in mask:
-				map[y][x] = replace_with
+				map[y][x] = internal_map[y][x]
 	for y in len(internal_map):
 		for x in len(internal_map[y]):
 			if internal_map[y][x] in mask:
 				map[y][x] = internal_map[y][x]
+				# Correct entity corresponding to the specific tile
+				for entity in entities:
+					if entity["position"].x == x and entity["position"].y == y:
+						entities.erase(entity)
+						break
+	# Correct entities
 	for i in range(len(entities) - 1, -1, -1):
 		if entities[i]["character"] in mask:
 			entities.remove(i)
@@ -146,13 +164,16 @@ func apply_immovable_mask(level_data, mask, replace_with=" "):
 	return level_data
 
 # TODO: Write this if needed or make one function for tiles and entities
-func apply_max_tile_mask(level_data, mask, replace_with=" "):
+func apply_max_tile_mask(level_data, mask):
 	return level_data
 
-func apply_max_entity_mask(level_data, mask, replace_with=" "):
+# Maybe rework this in the future, depends on the usage needed
+func apply_max_entity_mask(level_data, mask):
+	var internal_level_data = _load_internal_level_data(level_data["level"])
+	var internal_map = internal_level_data["map"]
 	var map = level_data["map"]
 	var entities = level_data["entities"]
-	var entity_count = level_data["character_count"]
+	var entity_count = count_tiles(map)
 	for character in mask:
 		if entity_count[character] > mask[character]:
 			var difference = entity_count[character] - mask[character]
@@ -161,18 +182,18 @@ func apply_max_entity_mask(level_data, mask, replace_with=" "):
 				var entity = entities[i]
 				if entity["character"] == character:
 					var p = entity["position"]
-					map[p.y][p.x] = replace_with
-					entities.remove(i)
-					difference -= 1
+					if map[p.y][p.x] != internal_map[p.y][p.x]:
+						map[p.y][p.x] = internal_map[p.y][p.x]
+						entities.remove(i)
+						difference -= 1
 	return level_data
 
-func parse_level_data(content: String):
+func parse_level_data(content: String, fill_missing: String = " "):
 	var section = "Global"
 	var lines = content.split("\n")
 	var map = []
-	var conf = []
+	var settings = {}
 	var entities = []
-	var character_count = {}
 	var map_row_index = 0
 	var level_name = lines[0].substr(1)
 	for line in lines:
@@ -183,18 +204,23 @@ func parse_level_data(content: String):
 			section = line.substr(1, len(line) - 2)
 			continue
 		if section == "Map" and map_row_index != map_height:
-			map.append(line.substr(0, map_width))
+			var map_row = line.substr(0, map_width)
+			# Fill a line with dummy characters if it is not long enough
+			if len(map_row) < map_width:
+				map_row += fill_missing.repeat(map_width - len(map_row))
+			map.append(map_row)
 			map_row_index += 1
-		elif section == "Conf":
-			line = line.split("=", false)
-			if line.size() == 2:
-				conf.append({line[0]:line[1]})
+		elif section == "Settings":
+			var pair = line.split("=", false)
+			if pair.size() == 2:
+				settings[pair[0]] = pair[1]
+	# Fill map vertically if there are missing lines
+	if len(map) < map_height:
+		for i in (map_height - len(map)):
+			map.append(fill_missing.repeat(map_width))
 	for y in len(map):
 		for x in len(map[y]):
 			var character = map[y][x]
-			if not character in character_count:
-				character_count[character] = 0
-			character_count[character] += 1
 			if character in map_entities:
 				entities.append({
 					"character": character,
@@ -204,16 +230,14 @@ func parse_level_data(content: String):
 	return {
 		"level": level_name,
 		"map": map,
-		"conf": conf,
-		"entities": entities,
-		"character_count": character_count
+		"settings": settings,
+		"entities": entities
 	}
 	
 func write_level_data(level, serialized_level_data):
 	var file_path = _get_level_external_path(level)
 	_write_file(file_path, serialized_level_data)
 
-# TODO: Reflect changes in conf too
 func serialize_level_data(level_data):
 	var section = "Global"
 	var internal_level_content = _read_file(_get_level_internal_path(level_data["level"]))
@@ -221,10 +245,19 @@ func serialize_level_data(level_data):
 	var map_row_index = 0
 	for i in len(lines):
 		var line = lines[i]
+		line = line.strip_edges()
+		if line == "": continue
+		if line.begins_with(";"): continue
 		if line.begins_with("[") and line.ends_with("]"):
 			section = line.substr(1, len(line) - 2)
 			continue
 		if section == "Map" and map_row_index != map_height:
 			lines[i] = level_data["map"][map_row_index]
 			map_row_index += 1
+		elif section == "Settings":
+			var pair = line.split("=", false)
+			for option in level_data["settings"]:
+				if option == pair[0]:
+					pair[1] = level_data["settings"][option]
+			lines[i] = pair.join("=")
 	return lines.join("\n")
