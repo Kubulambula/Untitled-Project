378ed44c0dd67e67e126f141fadda0ba8aa4c2f6
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-02-06 19:13:48 +0100

    Use Object's name property

--- Scripts/Entities/Door.gd
+++ Scripts/Entities/Door.gd
@@ -3,5 +3,5 @@ extends Area2D
 # TODO : redo the Door scene
 
 func _on_Area2D_body_entered(body):
-	if body is KinematicBody2D and body.has_method("recalculate_movement_settings"): # This is so retarded
+	if body.name == "Player":
 		EventReporter.report_event(self, "player_reached_door")
