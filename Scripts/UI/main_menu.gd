4550352a99f816322032e714bce88ca1c8131d37
Skulaurun Mrusal <Skulaurun@seznam.cz>
2022-02-07 00:14:08 +0100

    Make main menu load level from config

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -63,7 +63,7 @@ func _on_Button_pressed():
 	yield(WindowOverlay, "complete")
 	EscOverlay.allowed = true
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://Resources/Levels/Level1/level1.tscn")
+	get_tree().change_scene(LevelManager.get_level_scene(GameState.current_level))
 	WindowOverlay.undim(0.5)
 
 
